c905719c0ceac786c9a3d37b731ed89ad29e2c06
Viktoriia Krutovskaia <viktoriakrutovskaa408@gmail.com>
2020-01-24 23:44:32 +0400

    End task in the feature branch

---
+++ src/main/java/com/epam/izh/rd/online/service/SimpleSchoolBookService.java
@@ -0,0 +1,58 @@
+package com.epam.izh.rd.online.service;
+
+import com.epam.izh.rd.online.entity.Author;
+import com.epam.izh.rd.online.entity.SchoolBook;
+import com.epam.izh.rd.online.repository.BookRepository;
+
+public class SimpleSchoolBookService implements BookService {
+    private BookRepository<SchoolBook> schoolBookBookRepository;
+    private AuthorService authorService;
+
+    public SimpleSchoolBookService() {
+    }
+
+    public SimpleSchoolBookService(BookRepository<SchoolBook> schoolBookBookRepository, AuthorService authorService) {
+        this.schoolBookBookRepository = schoolBookBookRepository;
+        this.authorService = authorService;
+    }
+
+    @Override
+    public boolean save(SchoolBook book) {
+        if (authorService.findByFullName(book.getAuthorName(), book.getAuthorLastName()) == null) {
+            return false;
+        } else {
+            schoolBookBookRepository.save(book);
+            return true;
+        }
+    }
+
+    @Override
+    public SchoolBook[] findByName(String name) {
+
+        return schoolBookBookRepository.findByName(name);
+    }
+
+    @Override
+    public int getNumberOfBooksByName(String name) {
+        return schoolBookBookRepository.findByName(name).length;
+    }
+
+    @Override
+    public boolean removeByName(String name) {
+        return schoolBookBookRepository.removeByName(name);
+    }
+
+    @Override
+    public int count() {
+        return schoolBookBookRepository.count();
+    }
+
+    @Override
+    public Author findAuthorByBookName(String name) {
+        SchoolBook[] foundAuthor = schoolBookBookRepository.findByName(name);
+        if (foundAuthor.length == 0)
+            return null;
+        return authorService.findByFullName(foundAuthor[0].getAuthorName(), foundAuthor[0].getAuthorLastName());
+    }
+}
+
